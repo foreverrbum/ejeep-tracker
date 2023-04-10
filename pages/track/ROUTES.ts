70f63c0fefe1fae73f7a709f0e9af463cefc1763
foreverrbum <nicole.respecia123@gmail.com>
2023-04-10 15:13:27 +0800

    edit routefills

--- pages/track/ROUTES.ts
+++ pages/track/ROUTES.ts
@@ -4,8 +4,8 @@ export const LINE_A = [
 		label: 'Grade School',
 		title: 'Grade School',
 		coordinate: {
-			latitude: 14.634428736676266,
-			longitude: 121.0764358420065
+			latitude: 14.634782213092796,
+			longitude: 121.07616865459035
 		},
 		color: '#EAB95A'
 	},
@@ -42,6 +42,15 @@ export const LINE_A = [
 		coordinate: {
 			latitude: 14.636676879666329,
 			longitude: 121.07818357693269
+		},
+		color: '#EAB95A'
+	},
+	{
+		label: 'Hagdan',
+		title: 'Hagdan',
+		coordinate: {
+			latitude: 14.636027051065314,
+			longitude: 121.08066658895413
 		},
 		color: '#EAB95A'
 	}
@@ -88,15 +97,15 @@ const LINE_B = [
 export const MARKERS = _.uniq(_.concat(LINE_A, LINE_B))
 
 export const POLYLINE_A = [
-	{ latitude: 14.637864756965353, longitude: 121.07495470477606 },
-	{ latitude: 14.639942460975785, longitude: 121.07872004372351 },
-	{ latitude: 14.636676879666329, longitude: 121.07818357693269 },
-	{ latitude: 14.635939799891771, longitude: 121.07637196390262 },
-	{ latitude: 14.634428736676266, longitude: 121.0764358420065 },
-	{ latitude: 14.634828460124702, longitude: 121.07612551939081 },
-	{ latitude: 14.634873401327047, longitude: 121.07627001251339 },
-	{ latitude: 14.636334310647612, longitude: 121.07625042036516 },
-	{ latitude: 14.637272017219226, longitude: 121.07643066819381 },
-	{ latitude: 14.637651142724838, longitude: 121.07634707499543 }
+	{ latitude: 14.637864756965353, longitude: 121.07495470477606 }, // 0 gate 2.5
+	{ latitude: 14.640833122286619, longitude: 121.07629298277074 }, // 1 leong
+	{ latitude: 14.639942460975785, longitude: 121.07872004372351 }, // 2 xavier
+	{ latitude: 14.636676879666329, longitude: 121.07818357693269 }, // 3 old comms
+	{ latitude: 14.636027051065314, longitude: 121.08066658895413 }, // 4 hagdan
+	{ latitude: 14.634832289115554, longitude: 121.07512376349462 }, // gs other side
+	{ latitude: 14.634873401327047, longitude: 121.07627001251339 }, // 6 GS - middle road to highway
+	{ latitude: 14.636334310647612, longitude: 121.07625042036516 }, // 7 ocampo field small curve
+	{ latitude: 14.637272017219226, longitude: 121.07643066819381 }, // 8 ocampo field bigger curve
+	{ latitude: 14.637651142724838, longitude: 121.07634707499543 } // 9 jsec
 ]
 export const POLYLINE_B = LINE_B.map((line) => line.coordinate)
--- pages/track/RouteFill.tsx
+++ pages/track/RouteFill.tsx
@@ -1,6 +1,6 @@
 import { POLYLINE_A, POLYLINE_B } from './ROUTES'
 import { API_KEY } from './MAP'
-
+import { Polyline, Marker } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 
 export const RouteFill = () => {
@@ -14,26 +14,49 @@ export const RouteFill = () => {
 				strokeWidth={2}
 				strokeColor="#EAB95A"
 			/>
+			{/* <Marker key="213241" title="4" coordinate={POLYLINE_A[5]}></Marker> */}
 			<MapViewDirections
 				origin={POLYLINE_A[5]}
 				destination={POLYLINE_A[0]}
 				mode="WALKING"
-				waypoints={(POLYLINE_A[6], POLYLINE_A[7], POLYLINE_A[8], [POLYLINE_A[9]])}
+				waypoints={(POLYLINE_A[7], POLYLINE_A[8], [POLYLINE_A[9]])}
 				apikey={API_KEY} // insert your API Key here
 				strokeWidth={2}
 				strokeColor="#EAB95A"
 			/>
 			<MapViewDirections
-				origin={{ latitude: 14.6444700324687, longitude: 121.08041895372087 }}
-				destination={{ latitude: 14.6444700324687, longitude: 121.08041895372087 }}
-				waypoints={[{ latitude: 14.640826314486352, longitude: 121.07587336440143 }, POLYLINE_B[POLYLINE_B.length - 1]]}
+				origin={POLYLINE_B[0]}
+				destination={POLYLINE_B[POLYLINE_B.length - 2]}
+				apikey={API_KEY} // insert your API Key here
+				strokeWidth={2}
+				strokeColor="#8F7D40"
+			/>
+
+			<MapViewDirections
+				origin={POLYLINE_B[0]}
+				destination={POLYLINE_B[0]}
+				waypoints={[POLYLINE_B[POLYLINE_B.length - 1]]}
+				apikey={API_KEY} // insert your API Key here
+				strokeWidth={2}
+				strokeColor="#8F7D40"
+			/>
+			<Polyline
+				coordinates={[
+					{ latitude: 14.641350899262309, longitude: 121.07656149001258 }, // road in front of NW PArking
+					{ latitude: 14.641162571320805, longitude: 121.07634861944705 } // uturn back to univ road
+				]}
+				strokeColor="#8F7D40"
+				strokeWidth={2}
+			/>
+
+			<MapViewDirections
+				origin={{ latitude: 14.641404856160548, longitude: 121.07723036176739 }} // intersection before turning left to univ road
+				destination={{ latitude: 14.641162571320805, longitude: 121.07634861944705 }} // uturn back to univ road
 				apikey={API_KEY} // insert your API Key here
 				strokeWidth={2}
 				strokeColor="#8F7D40"
 				mode="WALKING"
 			/>
-
-			{/* { latitude: 14.640826314486352, longitude: 121.07587336440143 }, */}
 		</>
 	)
 }
